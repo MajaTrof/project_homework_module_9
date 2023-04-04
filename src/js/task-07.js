306c584994210ea3d48996f3194b7a428f882566
Nataliia <majatrof@gmail.com>
2023-04-04 13:23:24 +0200

    f

--- src/js/task-07.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
-
-input.addEventListener("input", changeText);
-
-function changeText() {
-    text.style.fontSize = input.value + 'px';
-  }
